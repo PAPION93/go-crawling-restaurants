649cc1521ffa8b51c12e27dd91f615c07d5ce8ba
PAPION93 <sjun1319@gmail.com>
2020-11-27 17:48:52 +0900

    update

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 func main() {
-	fmt.Println("Hello crawling")
+	Scrape()
+}
+
+// Scrape diningcode
+func Scrape() {
+
+	query := "만촌역"
+	page := 1
+
+	// Request the HTML page.
+	res, err := http.Get("https://www.diningcode.com/list.php?query=" + query + "&page=" + strconv.Itoa(page))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find the review items
+	doc.Find("#div_list li").Each(func(i int, s *goquery.Selection) {
+		name := s.Find(".btxt").Text()
+		menu := s.Find(".stxt").Text()
+		address := s.Find(".ctxt").Text()
+
+		fmt.Printf("Name: %s, Menu: %s, Address: %s\n", name, menu, address)
+	})
 }
