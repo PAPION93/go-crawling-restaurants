ca5b18dab38159e67fd528661a656b1cf90bf043
PAPION93 <sjun1319@gmail.com>
2020-12-02 20:08:43 +0900

    Add file logger

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/spf13/viper"
@@ -22,13 +25,25 @@ func init() {
 func main() {
 
 	a := database.Config{}
-	// a.DBInit()
-	a.TestDBInit()
+	a.DBInit()
+	// a.TestDBInit()
 	defer a.DBClose(a.DB)
 	a.DB.AutoMigrate(&domain.Restaurant{})
 
+	fpLog, err := os.OpenFile("logfile.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer fpLog.Close()
+
+	// 파일로그로 변경
+	log.SetOutput(fpLog)
+	log.Println("Start")
+
 	rr := repository.NewRestaurantRepository(a.DB)
 	ur := usecase.NewRestaurantUsecase(rr)
+
 	diningcodeRepo := diningcode.NewDiningcode(ur)
 	diningcodeRepo.Crawl()
+	log.Println("End")
 }
